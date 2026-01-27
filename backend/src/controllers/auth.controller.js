07edd5bde032ee20c8404a3cd1a09c9115c249ab
prateekwithcode <parteekkhatri2004@gmail.com>
2026-01-27 20:05:57 +0530

    add login,logout functionlities

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
-import User from "./models/User.model.js"
+import User from "../models/User.model.js"
 import jwt from "jsonwebtoken";
 import cookie from "cookie-parser";
+import e from "express";
 
 export async function signup(req,res){
     const {email,password,fullName}=req.body;
@@ -30,7 +31,7 @@ export async function signup(req,res){
         const idx = Math.floor(Math.random() * 100) + 1;
         const randomAvator = `https://avatar.iran.liara.run/public/${idx}.png`;
 
-        const newUser = new User.create({
+        const newUser = await User.create({
             email,
             password,
             fullName,
@@ -51,12 +52,42 @@ export async function signup(req,res){
         res.status(201).json({success:true,user:newUser})
 
     } catch (error) {
-        
+        console.log("Error in signup controller",error);
+        res.send(500).json({message:"Internal Server Error"});
     }
 }
-export function login(req,res){
-    res.send("login");
+export async function login(req,res){
+    try {
+        const {email,password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({message:"All field are required"});
+        }
+
+        const user = await User.findOne({email});
+        if(!user) return res.status(401).json({message:"Invalid email or password"});  
+        
+        const isPasswordCorrect = await user.matchPassword(password);
+        if(!isPasswordCorrect){
+            return res.status(401).json({message:"Invalid email or password"})
+        }
+        const token = jwt.sign({userId:user._id},process.env.JWT_SECRET_KEY,{
+            expiresIn:"7d",
+        })
+
+        res.cookie("jwt",token,{
+            maxAge:7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,  // prevent XSS attacks
+            sameSite:"strict",  // prevent CSRF attacks
+            secure:process.env.NODE_ENV==="production"
+        })
+
+        res.status(200).json({success:true,user})
+    } catch (error) {
+        console.log("Error in login controller",error.message);
+        res.status(500).json({message:"Internal server error"})
+    }
 }
-export function logout(req,res){
-    res.send("signout");
+export async function logout(req,res){
+    res.clearCookie("jwt");
+    res.status(200).json({success:true,message:"Logout successful"});
 }
--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -1,70 +1,69 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-    fullName:{
-        type:String,
-        require:true,
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      require: true,
     },
-    email:{
-        type:String,
-        require:true,
-        unique:true,
+    email: {
+      type: String,
+      require: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        require:true,
-        minlength:6
+    password: {
+      type: String,
+      require: true,
+      minlength: 6,
     },
-    bio:{
-        type:String,
-        default:""
+    bio: {
+      type: String,
+      default: "",
     },
-    profilePic:{
-        type:String,
-        default:"",
+    profilePic: {
+      type: String,
+      default: "",
     },
-    nativeLanguage:{
-        type:String,
-        default:"",
+    nativeLanguage: {
+      type: String,
+      default: "",
     },
-    location:{
-        type:String,
-        default:"",
+    location: {
+      type: String,
+      default: "",
     },
-    isOnboarded:{
-        type:Boolean,
-        default:false,
+    isOnboarded: {
+      type: Boolean,
+      default: false,
     },
-    friends:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User",
-        }
-    ]
-},{timestamps:true})
-
-const User = mongoose.model("User",userSchema);
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true },
+);
 
 // pre hook
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  } catch (error) {
+    next(error);
+  }
+});
 
-userSchema.pre("save",async function(next) {
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  const isPasswordCorrect = await bcrypt.compare(enteredPassword,this.password);
+  return isPasswordCorrect;
+}
 
-    if(!this.isModified("password")) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password =await bcrypt.hash(this.password,salt);
-        next();
-    } catch (error) {
-        next(error)
-    }
-}())
+const User = mongoose.model("User", userSchema);
 
 
 export default User;
-
-
-
-
-
-
