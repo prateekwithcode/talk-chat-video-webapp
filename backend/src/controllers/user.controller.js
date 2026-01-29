bb92c32064989365b011f7b1538d8fe9402f8e02
prateekwithcode <parteekkhatri2004@gmail.com>
2026-01-29 21:11:57 +0530

    create send friend request to user

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/User.model.js";
+import FriendRequest from "../models/FriendRequest.model.js";
+
 export async function getRecommendedUsers(req, res) {
   try {
     const currentUserId = req.user._id;
@@ -29,5 +31,53 @@ export async function getMyFriends(req, res) {
   } catch (error) {
     console.error("Error in getMyFriends controller", error.message);
     res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function sendFriendRequest(req, res) {
+  try {
+    const myId = req.user.id;
+    const { id: recipientId } = req.params;
+
+    // prevent sending request to yourself
+    if (myId === recipientId) {
+      return res
+        .status(400)
+        .json({ message: "you can't send friend request to yourself" });
+    }
+
+    const recipient = await User.finById(recipientId);
+    if (!recipient) {
+      return res.status(404).json({ message: "Recipient not Found" });
+    }
+
+    // check if user is already friends
+    if (recipient.friends.includes(myId)) {
+      return res
+        .status(400)
+        .json({ message: "you are already friends with this user" });
+    }
+
+    // check if a req already exist
+    const existingRequest = await FriendRequest.findOne({
+      $or: [
+        { sender: myId, recipient: recipientId },
+        { sender: recipientId, recipient: myId },
+      ],
+    });
+
+    if (existingRequest) {
+      return res.status(400).json({
+        message: "A friend request already exists between you and this user",
+      });
+    }
+
+    const friendRequest = await FriendRequest.create({
+      sender: myId,
+      recipient: recipientId,
+    });
+    res.status(200).json(friendRequest);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
   }
 }
