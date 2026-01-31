62d899d34204b1a4d2428f064720f5645a081a01
prateekwithcode <parteekkhatri2004@gmail.com>
2026-01-31 09:24:07 +0530

    feat: update frontend dependencies and implement routing
    - Added Tailwind CSS and React Query for styling and data fetching.
    - Removed unused assets and CSS files.
    - Refactored App component to include routing for multiple pages.
    - Created new pages: HomePage, LoginPage, SignUpPage, CallPage, ChatPage, NotificationPage, OnboardingPage.
    - Set up Axios instance for API calls.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,28 @@
 import express from "express";
 import "dotenv/config";
-import {connectDB} from "./lib/dbs.js";
+import { connectDB } from "./lib/dbs.js";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/auth.route.js";
-import chatRoutes from "./routes/user.route.js"
+import chatRoutes from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT;  
+
+app.use(cors({ 
+    origin: "http://localhost:5173",
+    credentials: true  //allow frontend to send a cookies 
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth/",authRoutes);
-app.use("api/user/",userRoutes);
-app.use("api/chat/",chatRoutes);
+app.use("/api/auth/", authRoutes);
+app.use("api/user/", userRoutes);
+app.use("api/chat/", chatRoutes);
 
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-    connectDB();
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+  connectDB();
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import NotificationPage from "./pages/NotificationPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import { useQuery } from "@tanstack/react-query";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
+import { axiosInstance } from "./lib/axios.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todos"],
 
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;  
+    },
+    retry:false,
+  });
+  console.log({data});
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="h-screen">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notification" element={<NotificationPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
